0b3274c687426848563dd16db7f7e121d738f2d4
Matthew Sital-Singh <mattss@netsight.co.uk>
2016-05-13 17:02:46 +0100

    Refactor weather checking code

--- raincheck.py
+++ raincheck.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import render_template
+from weather import update_weather_data
+
 app = Flask(__name__)
 
 
+@app.route("/check/<key>")
+def checkit(key):
+    latitude = 51.45
+    longitude = 2.5
+    weather = update_weather_data(key, latitude, longitude)
+    return render_template('hello.html', weather=weather)
+
+
 @app.route("/")
-def hello():
-    return render_template('hello.html')
+def home():
+    return render_template('hello.html', weather='')
 
 if __name__ == "__main__":
     app.run()
---
+++ weather.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+
+API_URL = 'https://api.forecast.io/forecast'
+
+
+def update_weather_data(api_key, lat, lng):
+    url = "{base}/{key}/{lat},{lng}".format(
+        base=API_URL,
+        key=api_key,
+        lat=lat,
+        lng=lng,
+    )
+    response = requests.get(url)
+    return response.json
+
+if __name__ == "__main__":
+    api_key = sys.argv[1]
+    result = update_weather_data(api_key, 52, 2.5)
+    print(result)
